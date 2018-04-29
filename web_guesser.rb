d1900f05fd25760441337bfe77433db725442bb3
Michael Gervasoni <michaelgervasoni@gmail.com>
2018-04-28 21:54:32 -0700

    I3: number guessing game complete

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,31 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-randNum = rand(100)
+RANDNUM = rand(100)
+@@tries = -1
 get '/' do
-  erb :index, :locals => {:randNum => randNum}
+  @@tries = @@tries + 1
+  guess = params[:guess]
+  message = check(guess)
+  if params[:resetit] == "reset"
+    resetit
+  end
+  erb :index, :locals => {:randNum => RANDNUM, :message=> message, :tries => @@tries}
 #"renders the ERB template named index and creates a local variable for the template named number which has the same value as the number variable from this server code."
+end
+def check(num)
+  if num.to_i == RANDNUM
+    message = "You got it!!"
+  elsif num.to_i < RANDNUM
+    message = "Too low!"
+  elsif num.to_i > RANDNUM
+    message = "Too high!"
+  else
+    message = "Did you enter a number?"
+  end
+  return message
+end
+
+def resetit
+  @@tries = 0
 end
